49ba231de81d355ab2d81ed392fe146b1e356b92
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-04-25 22:48:36 +0300

    close #62

--- bot/src/main/kotlin/me/y9san9/prizebot/handlers/choosen_inline_result/commands/SendGiveawayCommand.kt
+++ bot/src/main/kotlin/me/y9san9/prizebot/handlers/choosen_inline_result/commands/SendGiveawayCommand.kt
@@ -1,12 +1,17 @@
 package me.y9san9.prizebot.handlers.choosen_inline_result.commands
 
 import me.y9san9.prizebot.extensions.telegram.PrizebotChosenInlineResultUpdate
+import me.y9san9.telegram.updates.extensions.command.command
 
 
 object SendGiveawayCommand {
     fun handle(update: PrizebotChosenInlineResultUpdate) {
+        val giveawayId = update.command(splitter = "_")
+            ?.args?.getOrNull(index = 0)
+            ?.toLongOrNull() ?: return
+        
         update.di.addActiveMessage (
-            giveawayId = update.resultId.toLongOrNull() ?: return,
+            giveawayId = giveawayId,
             inlineMessageId = update.inlineMessage ?: return
         )
     }
