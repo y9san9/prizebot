6b73dfc0e97f068700c52827f38b3880203293e5
Alex Sokol <sokolsokolinskiy@gmail.com>
2021-05-18 21:01:25 +0300

    little reformat

--- db-migrations/src/main/kotlin/me/y9san9/db/migrations/MigrationsConfig.kt
+++ db-migrations/src/main/kotlin/me/y9san9/db/migrations/MigrationsConfig.kt
@@ -3,9 +3,10 @@ package me.y9san9.db.migrations
 import org.jetbrains.exposed.sql.SchemaUtils
 
 
-class MigrationsConfig (
+class MigrationsConfig {
     // Table name for tracking versions in database
-    var tableName: String = "migrations",
+    var tableName: String = "migrations"
+
     /**
      * if null, then by default module will assume that the table is already up-to-date
      * Example: you have just deployed your new bot instance and all tables were created
@@ -13,4 +14,4 @@ class MigrationsConfig (
      * from latest [DatabaseMigration.afterVersion] or will keep it null if there is no migrations yet
      */
     var defaultVersion: Int? = null
-)
+}
