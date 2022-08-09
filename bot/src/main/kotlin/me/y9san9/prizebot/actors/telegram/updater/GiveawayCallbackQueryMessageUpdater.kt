95ee29ec404428d5e0a8e2e846e6bed8ecee26f9
Alex Sokol <y9san9@gmail.com>
2022-08-09 14:45:26 +0300

    feat(close #100): participate action optimized

--- bot/src/main/kotlin/me/y9san9/prizebot/actors/telegram/updater/GiveawayCallbackQueryMessageUpdater.kt
+++ bot/src/main/kotlin/me/y9san9/prizebot/actors/telegram/updater/GiveawayCallbackQueryMessageUpdater.kt
@@ -2,6 +2,14 @@ package me.y9san9.prizebot.actors.telegram.updater
 
 import dev.inmo.micro_utils.coroutines.safelyWithoutExceptions
 import dev.inmo.tgbotapi.extensions.api.edit.text.editMessageText
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancelAndJoin
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.plus
 import me.y9san9.prizebot.database.giveaways_storage.Giveaway
 import me.y9san9.prizebot.actors.telegram.extractor.GiveawayFromCommandExtractor
 import me.y9san9.prizebot.extensions.telegram.PrizebotCallbackQueryUpdate
@@ -11,7 +19,29 @@ import me.y9san9.telegram.extensions.asTextContentMessage
 
 object GiveawayCallbackQueryMessageUpdater {
 
-    suspend fun update (
+    private val coroutineScope = CoroutineScope(Dispatchers.IO) + SupervisorJob()
+    private val giveawayEdits = Channel<GiveawayEdit>()
+
+    init {
+        coroutineScope.launch {
+            val pendingEdits = mutableListOf<GiveawayEdit>()
+
+            for (edit in giveawayEdits) {
+                // cancel all the previous edits
+                pendingEdits
+                    .filter { pendingEdit -> pendingEdit isSame edit }
+                    .forEach { pendingEdit -> pendingEdit.job.cancelAndJoin() }
+
+                edit.job.invokeOnCompletion {
+                    pendingEdits.removeAll { pendingEdit -> pendingEdit.job === edit.job }
+                }
+
+                pendingEdits += edit
+            }
+        }
+    }
+
+    suspend fun update(
         update: PrizebotCallbackQueryUpdate,
         demo: Boolean = false,
         splitter: String = "_"
@@ -20,7 +50,7 @@ object GiveawayCallbackQueryMessageUpdater {
         update(update, giveaway, demo)
     }
 
-    suspend fun update (
+    suspend fun update(
         update: PrizebotCallbackQueryUpdate,
         giveaway: Giveaway,
         demo: Boolean = false
@@ -28,25 +58,43 @@ object GiveawayCallbackQueryMessageUpdater {
         val inlineMessageId = update.inlineMessageId
         val message = update.message?.asTextContentMessage()
 
-        if(inlineMessageId == null && message == null)
-            return
-
         val (entities, markup) = giveawayContent(update.di, giveaway, demo)
 
-        if(inlineMessageId != null) safelyWithoutExceptions {
-            update.bot.editMessageText (
+        val edit = when {
+            inlineMessageId != null -> GiveawayEdit.Inline(
                 inlineMessageId = inlineMessageId,
-                entities = entities,
-                replyMarkup = markup
+                job = coroutineScope.launch {
+                    safelyWithoutExceptions {
+                        update.bot.editMessageText(inlineMessageId, entities, replyMarkup = markup)
+                    }
+                }
             )
+            message != null -> GiveawayEdit.Message(
+                messageId = message.messageId,
+                job = coroutineScope.launch {
+                    safelyWithoutExceptions {
+                        update.bot.editMessageText(message, entities, replyMarkup = markup)
+                    }
+                }
+            )
+            else -> return
         }
 
-        if(message != null) safelyWithoutExceptions {
-            update.bot.editMessageText (
-                message = message,
-                entities = entities,
-                replyMarkup = markup
-            )
+        coroutineScope.launch {
+            giveawayEdits.send(edit)
+        }
+    }
+
+    sealed class GiveawayEdit(val job: Job) {
+        abstract infix fun isSame(other: GiveawayEdit): Boolean
+
+        class Message(private val messageId: Long, job: Job) : GiveawayEdit(job) {
+            override fun isSame(other: GiveawayEdit) =
+                other is Message && messageId == other.messageId
+        }
+        class Inline(private val inlineMessageId: String, job: Job) : GiveawayEdit(job) {
+            override fun isSame(other: GiveawayEdit) =
+                other is Inline && inlineMessageId == other.inlineMessageId
         }
     }
 }
